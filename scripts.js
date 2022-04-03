00495b471b52a02fe87dfc657afc4abde2684a22
MartynasTv <73237433+Marcynas@users.noreply.github.com>
2022-04-03 13:22:17 +0300

    main

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
 
+    document.getElementById("RegisterButton").onclick = function () {
+        location.href = "/Puslapiai/Prisijungimas/registruotis.html";
+    };
+    document.getElementById("LoginButton").onclick = function () {
+        location.href = "/Puslapiai/Prisijungimas/prisijungti.html";
+    };
+    document.getElementById("SubmitLogin").onclick = function () {
+        location.href = "/Puslapiai/index.html";
+    };
