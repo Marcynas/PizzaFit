e7301549d5f0d222649a8dd3b6c69df825bf158f
MartynasTv <73237433+Marcynas@users.noreply.github.com>
2022-04-05 17:00:54 +0300

    main

--- scripts.js
+++ scripts.js
@@ -2,10 +2,10 @@
   window.onload = function(){ 
   
   document.getElementById("RegisterButton").onclick = function () {
-        location.href = "/Puslapiai/Prisijungimas/registruotis.html";
+        location.href = "/Prisijungimas/registruotis.html";
     };
     document.getElementById("LoginButton").onclick = function () {
-        location.href = "/Puslapiai/Prisijungimas/prisijungti.html";
+        location.href = "/Prisijungimas/prisijungti.html";
     };
     document.getElementById("BackHomeButton").onclick = function () {
         console.log("BackHomeButton");
